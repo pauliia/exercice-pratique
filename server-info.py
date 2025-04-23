b8d92087cffccb6892aac6109b843283d7cd6737
paul butticaz <paul.butticaz@iia-formation.fr>
2025-04-23 16:56:33 +0200

    Adaptation du script server-info.py pour Linux v2

--- server-info.py
+++ server-info.py
@@ -2,7 +2,7 @@
 import psutil
 
 # Get the memory usage information
-memory = psutil.virtual_memory()
+memory = psutil.virtualMemory()
 
 # Print the memory usage details
 print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3)))
